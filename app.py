fdea9208640090e248709e4252b7bad4117e8974
XimenaZepedaRuiz <zepedax30@gmail.com>
2026-09-01 17:51:55 -0600

    Implementar registro de gastos

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
+gastos = []
+
 print("Sistema de Control de Gastos Personales")
-print("Proyecto iniciado correctamente.")
+
+descripcion = input("Ingresa la descripción del gasto: ")
+cantidad = float(input("Ingresa la cantidad del gasto: "))
+
+gasto = {
+    "descripcion": descripcion,
+    "cantidad": cantidad
+}
+
+gastos.append(gasto)
+
+print("Gasto registrado correctamente.")
+print(f"Gasto: {descripcion} - ${cantidad:.2f}")
